4ec942b2c24851dccb83a4664b60f9b02cbc3968
Yogi Prasetya <yogi.prasetya@digitaltelkom.id>
2025-11-08 13:18:17 +0700

    setup preview storrybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,27 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
   addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
     "@storybook/addon-onboarding",
+    "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
+    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+  docs: {},
+  typescript: {
+    check: false,
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 };
+
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react-vite";
+import type { Preview } from "@storybook/react";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
@@ -8,7 +9,21 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: "light",
+      values: [
+        {
+          name: "light",
+          value: "#ffffff",
+        },
+        {
+          name: "dark",
+          value: "#1a1a1a",
+        },
+      ],
+    },
   },
+  tags: ["autodocs"],
 };
 
 export default preview;
