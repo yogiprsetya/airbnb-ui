0ba38936dd20ff0982e32dac65b7676cf00e4891
Yogi Prasetya <yogi.prasetya@digitaltelkom.id>
2025-11-13 21:37:30 +0700

    setup npm repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 // Export all components
+export * from './components/accordion';
+export * from './components/button';
+export * from './components/checkbox';
+export * from './components/input';
+export * from './components/label';
+export * from './components/radio-button';
 
 // Export utilities
-export * from './lib/utils';
+// export * from './lib/utils';
 
 // Import styles
 import './index.css';
